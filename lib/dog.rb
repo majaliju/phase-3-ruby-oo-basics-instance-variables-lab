e60076229675c9a8f0a8613bd21f3a24c58e2d7d
majaliju <majalijunyc@gmail.com>
2022-07-30 19:05:37 -0400

    updated

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+  # setter method
+  def name=(dog_name)
+    # assigns the @name instance variable a value of whatever is passed as an arg
+    @this_dogs_name = dog_name
+  end
 
+  # getter method
+  def name
+    # accesses the data from the @name instance variable and returns it
+   @this_dogs_name
+  end
 end
